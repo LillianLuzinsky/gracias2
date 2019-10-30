b178c7b7ac43203bd8effac6202f78f896e61d8d
Lillian Luzinsky <eyeris@tpg.com.au>
2019-10-30 18:43:53 +1100

    added questions model, migrated and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Question.create(question_number: 'what were you grateful for?')
+Question.create(question_number: 'who made you feel grateful?')
+Question.create(question_number: 'how did you show your appreciation?')
