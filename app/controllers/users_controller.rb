a861907591368fb5096dd1890e4b98d4b5cc1a7d
Lillian Luzinsky <eyeris@tpg.com.au>
2019-10-31 17:24:05 +1100

    fixed user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+
+    puts "=============================="
+    puts params.inspect
+    puts "=============================="
+
+    form_params = params.require(:user)
+      .permit(:name, :email, :password, :password_confirmation)
+
+    @user = User.new(form_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_url, notice: "Thank you for signing up!"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-    has_many :answers
+  has_many :answers
     # attr_accessible :email, :password, :password_confirmation
     # validates_uniqueness_of :email
 end
